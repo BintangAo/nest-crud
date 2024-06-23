3badd363d908994f34dc580c691af92878e155ac
BintangAo <valmero33@gmail.com>
2024-06-23 23:22:18 +0700

    Change try-catch block into catch method, fix getOneTask

--- src/tasks/select-tasks.service.ts
+++ src/tasks/select-tasks.service.ts
@@ -5,43 +5,47 @@ import {
 } from '@nestjs/common';
 import { eq } from 'drizzle-orm';
 import { db } from 'src/db';
-import { SelectTasks, SelectUsers, tasksTable, usersTable } from 'src/schema';
+import { SelectTasks, SelectUsers, usersTable } from 'src/schema';
 
 @Injectable()
 export class SelectTaskService {
   async getOneTask(
-    id: SelectTasks['id'],
+    taskId: SelectTasks['id'],
     userToken: SelectUsers['token'],
   ): Promise<SelectTasks> {
-    try {
-      const user = await db.query.usersTable.findFirst({
-        where: eq(usersTable.token, userToken),
-        with: {
-          tasks: {
-            where: eq(tasksTable.id, id),
-          },
-        },
-      });
-      if (!user.tasks[0]) {
-        throw new NotFoundException();
-      } else {
-        return user.tasks[0];
-      }
-    } catch {
-      throw new InternalServerErrorException('Failed to fetch to the database');
-    }
-  }
-  async getAllTasks(userToken: SelectUsers['token']): Promise<SelectTasks[]> {
-    try {
-      const user = await db.query.usersTable.findFirst({
+    const user = await db.query.usersTable
+      .findFirst({
         where: eq(usersTable.token, userToken),
         with: {
           tasks: true,
         },
+      })
+      .catch(() => {
+        throw new InternalServerErrorException(
+          'Failed to fetch to the database',
+        );
       });
-      return user.tasks;
-    } catch {
-      throw new InternalServerErrorException('Failed to fetch to the database');
+    if (!user.tasks.some(({ id }) => id === taskId)) {
+      throw new NotFoundException();
+    } else {
+      return user.tasks.find(({ id }) => {
+        return id === taskId;
+      });
     }
+  }
+  async getAllTasks(userToken: SelectUsers['token']): Promise<SelectTasks[]> {
+    const user = await db.query.usersTable
+      .findFirst({
+        where: eq(usersTable.token, userToken),
+        with: {
+          tasks: true,
+        },
+      })
+      .catch(() => {
+        throw new InternalServerErrorException(
+          'Failed to fetch to the database',
+        );
+      });
+    return user.tasks;
   }
 }
